89a09c3806e423c639d558d5ef04b4ab4aa984ad
maksim.malakhov <maksim.malakhov@bankismart.com>
2025-08-03 13:24:31 +0300

    remove dist and add to gitignore

--- dist/user/user.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { RegistrationResponseDto, RegisterDto } from './dto/user.dto';
-import { User } from './user.entity';
-export declare class UserService {
-    private userRepository;
-    constructor(userRepository: Repository<User>);
-    register(registerDto: RegisterDto): Promise<RegistrationResponseDto>;
-}
